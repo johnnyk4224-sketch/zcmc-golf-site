0f1a4ebec476ed58a7753acab72e2beda1f6cb77
johnnyk4224-sketch <johnnyk4224@gmail.com>
2026-03-12 13:21:41 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 window.ZCMC_CONFIG = {
-  GOOGLE_SHEETS_WEBHOOK: "https://script.google.com/macros/s/AKfycby9ZWTtXsPwi_WzwHhDrJqy_c8mbEK85md_K49UNAblSevYeL8iHDEN8KCYl9k_rQqV/exec"
+  GOOGLE_SHEETS_WEBHOOK: "https://script.google.com/macros/s/AKfycbww-85jXyOM4W1jhMUHJdghHBEgHPhjKoiWrIgbquTaDPTNCwyb5AIJ7hWPXk6Gz1Tm/exec"
 };
