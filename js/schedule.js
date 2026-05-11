4484ca8f310548ad1a22e9387b7e4ef28f4d3e98
johnnyk4224-sketch <johnnyk4224@gmail.com>
2026-05-11 07:09:38 -0500

    Update schedule.js

--- js/schedule.js
+++ js/schedule.js
@@ -22,10 +22,15 @@ async function loadSchedule() {
 
       const tr = document.createElement("tr");
       tr.innerHTML = `
-        <td>${row.date || ""}</td>
-        <td>${row.tee_time || ""}</td>
-        <td>${row.team_a || ""}</td>
-        <td>${row.team_b || ""}</td>
+return {
+  night: nightName,
+  date: obj["Date"] || "",
+  tee_time: obj["TeeTime"] || "",
+  team_a: obj["TeamA"] || "",
+  team_b: obj["TeamB"] || "",
+  points_a: obj["PointsA"] || "",
+  points_b: obj["PointsB"] || ""
+};
       `;
       tbody.appendChild(tr);
     });
